9b4598c64746ad8498a2dc868b1580d871dadfb3
Adam Mazel <43005538+amaze2@users.noreply.github.com>
2021-01-07 20:45:55 -0500

    Update concordy_processor_term_over_time.py

--- concordy_processor_term_over_time.py
+++ concordy_processor_term_over_time.py
@@ -39,13 +39,9 @@ years = range(start_year,end_year+1)
 
 lem_ans = st.sidebar.selectbox(label="Lemmatize the data?", options=("Yes","No"), index=0)
 if lem_ans == "Yes":
-    try:
-        pickleIn = open("concordy_lemma.pickle",'rb')
-        d_year_month_text = pickle.load(pickleIn)
-        search_terms = [lemmatizer.lemmatize(term.lower()) for term in search_terms if term.isalpha()]
-    except:
-        error = pickle.UnpicklingError(d_year_month_text)
-        print(error)
+    pickleIn = open("concordy_lemma.pickle",'rb')
+    d_year_month_text = pickle.load(pickleIn)
+    search_terms = [lemmatizer.lemmatize(term.lower()) for term in search_terms if term.isalpha()]
 else:
     pickleIn = open("concordy_raw_no_POS.pickle",'rb')
     d_year_month_text = pickle.load(pickleIn)
